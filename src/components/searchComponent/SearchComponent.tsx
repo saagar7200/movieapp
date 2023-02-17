760106ea81c1b334abe4a2b608ab993ac36525ab
sagar <rsaagar7200@gmail.com>
2023-02-17 21:16:02 +0545

    build error

--- src/components/searchComponent/SearchComponent.tsx
+++ src/components/searchComponent/SearchComponent.tsx
@@ -1,46 +1,40 @@
-import React, { useRef ,useState} from 'react';
-import { useQuery } from 'react-query';
-import './searchComponent.css'
+import React, { useRef, useState } from "react";
+import "./searchComponent.css";
 
-type Props = {}
+type Props = {};
 
 const SearchComponent = (props: Props) => {
+  const [keyword, setKeyword] = useState("");
 
-  const [keyword,setKeyword] = useState('')
+  const searchRef = useRef<any>();
 
-    const searchRef= useRef<any>();
-
-    const handleSearch = ()  =>{
-      
-      setKeyword("")
-      
-    }
-    const handleChange = () =>{
-       let key:string =searchRef.current?.value
-      setKeyword(key)
-    }
+  const handleSearch = () => {
+    setKeyword("");
+  };
+  const handleChange = () => {
+    let key: string = searchRef.current?.value;
+    setKeyword(key);
+  };
 
   return (
-    <div className='wrapper'>
-        <h1>
-        Welcome to the weekend watch
-        </h1>
-        <p>Millions of Movies, TV Shoes and many more</p>
+    <div className="wrapper">
+      <h1>Welcome to the weekend watch</h1>
+      <p>Millions of Movies, TV Shoes and many more</p>
 
-        <div className='btn-container'>
-            <input
-            ref={searchRef}
-            value={keyword}
-            type='text'
-            name='search'
-            placeholder='Search for movie, tv shoes, and person'
-            onChange={handleChange}
-            />
-                
-            <button onClick={handleSearch}>Search</button>
-        </div>
+      <div className="btn-container">
+        <input
+          ref={searchRef}
+          value={keyword}
+          type="text"
+          name="search"
+          placeholder="Search for movie, tv shoes, and person"
+          onChange={handleChange}
+        />
+
+        <button onClick={handleSearch}>Search</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchComponent
+export default SearchComponent;
--- src/context/MovieState.tsx
+++ src/context/MovieState.tsx
@@ -1,30 +1,14 @@
-import {useState} from "react";
-import {MovieContext} from "./MovieContext";
+import { useState } from "react";
+import { MovieContext } from "./MovieContext";
 
 interface IAuthProvider {
   children: any;
 }
 
-export const MovieProvider = ({children}: IAuthProvider) => {
+export const MovieProvider = ({ children }: IAuthProvider) => {
+  const [search] = useState("");
 
-  const [search, setSearch] = useState("");
-
-  
-
- 
-
-  // const value = {
-  //   //   splashIsLoading,
-  //   //   setSplashIsLoading,
-  //   //   isAuthenticated,
-  //   //   setIsAuthenticated,
-  //   //   setFirstLaunchToLocalStorage,
-  //   //   setIsAuthenticatedToLocalStorage,
-  //   launchedFirstTime,
-  //   setLaunchedFirstTime,
-  //   token,
-  //   setToken,
-  //   setTokenToLocalStorage,
-  // };
-  return <MovieContext.Provider value={search}>{children}</MovieContext.Provider>;
+  return (
+    <MovieContext.Provider value={search}>{children}</MovieContext.Provider>
+  );
 };
